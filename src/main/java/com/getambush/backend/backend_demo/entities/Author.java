1e9c7302b2a57008e186887b7a66a1853dfb07c1
Vander Alves <vlvsjr@gmail.com>
2025-05-08 15:26:18 -0300

    Add missing imports to entity files

--- src/main/java/com/getambush/backend/backend_demo/entities/Author.java
+++ src/main/java/com/getambush/backend/backend_demo/entities/Author.java
@@ -2,6 +2,7 @@ package com.getambush.backend.backend_demo.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
--- src/main/java/com/getambush/backend/backend_demo/entities/Book.java
+++ src/main/java/com/getambush/backend/backend_demo/entities/Book.java
@@ -2,6 +2,9 @@ package com.getambush.backend.backend_demo.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
