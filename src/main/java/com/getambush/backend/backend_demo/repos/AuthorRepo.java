055834ad47241e5e6f0f10a0cc3539c4f46058e1
Vander Alves <vlvsjr@gmail.com>
2025-05-08 11:01:27 -0300

    Add findByName method to Author repository

--- src/main/java/com/getambush/backend/backend_demo/repos/AuthorRepo.java
+++ src/main/java/com/getambush/backend/backend_demo/repos/AuthorRepo.java
@@ -4,7 +4,10 @@ import com.getambush.backend.backend_demo.entities.Author;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface AuthorRepo extends CrudRepository<Author, UUID> {}
+public interface AuthorRepo extends CrudRepository<Author, UUID> {
+    Optional<Author> findByName(String name);
+}
