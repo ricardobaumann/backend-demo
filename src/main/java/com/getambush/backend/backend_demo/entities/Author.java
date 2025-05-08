62865c238d5c762bd462005c0f14fb7b63f6bf90
Vander Alves <vlvsjr@gmail.com>
2025-05-08 10:02:06 -0300

    Create Author entity

---
+++ src/main/java/com/getambush/backend/backend_demo/entities/Author.java
@@ -0,0 +1,23 @@
+package com.getambush.backend.backend_demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "authors")
+public class Author {
+    @Id
+    private UUID id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+}
